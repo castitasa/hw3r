68f7e666fcb9378d43178cbad172c65be3c2f0da
castitasa <aibike0806@gmail.com>
2023-11-13 18:58:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [input1, setInput1] = useState(0);
+  const [input2, setInput2] = useState(0);
+  const [result, setResult] = useState(0);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input type="text" value={input1} onChange={(e)=>setInput1(e.target.value)} />
+      <input type="text" value={input2} onChange={(e)=>setInput2(e.target.value)}/>
+
+      <div>
+        <button onClick={()=>{
+            setResult(+input1 + +input2)
+        }}>+</button>
+        <button onClick={()=>{
+            setResult(+input1 - +input2)
+        }}>-</button>
+        <button onClick={()=>{
+            setResult(+input1 * +input2)
+        }}>*</button>
+        <button onClick={()=>{
+            setResult(+input1 / +input2)
+        }}>/</button>
+        <button onClick={()=>{
+            setResult(Math.pow(+input1, +input2))
+        }}>степень</button>
+        <button onClick={()=>{
+            setResult(Math.pow(+input1, 1/ +input2))
+        }}>корень</button>
+      </div>
+      <h1>{result}</h1>
     </div>
   );
 }
